a5615d916d9d93345d50224a1a1a6ad39fe580b1
Miljković Jovan <77690201+miljkovicjovan@users.noreply.github.com>
2025-04-05 01:57:40 +0200

    initial crud setup

---
+++ BlazorCrud/Data/BlazorCrudContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Data
+{
+    public class BlazorCrudContext : DbContext
+    {
+        public BlazorCrudContext (DbContextOptions<BlazorCrudContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorCrud.Data.Transaction> Transaction { get; set; } = default!;
+    }
+}
---
+++ BlazorCrud/Data/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorCrud.Data
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public string Type { get; set; } = "Expense";
+        public string Category { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.Today;
+    }
+}
